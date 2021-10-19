1e7d736c8884d9fe4b52eff49f5a9c3575ae283c
Meni Nuriel <nurielmeni@gmail.com>
2021-10-19 21:53:01 +0300

    s

--- config/params.php
+++ config/params.php
@@ -3,7 +3,7 @@
 return [
     'supplierId' => 'c968de21-7cd7-4baa-9c94-1b8891d396cc',
     'cvWebMail' => 'r2m-hr@cvwebmail.com',
-    'bccMail' => 'nurielmeni@gmail.com',
+    'bccMail' => '',
     'fromMail' => 'jobs@r2m.com',
     'fromName' => 'R2M Jobs',
     'defaultSid' => 'c968de21-7cd7-4baa-9c94-1b8891d396cc',
